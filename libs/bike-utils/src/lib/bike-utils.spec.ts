e32bdceb11813b4c4b60d1b32ecf97e336ad4e1b
Best <potikornth@gmail.com>
2021-08-03 10:45:44 +0700

    first init

---
+++ libs/bike-utils/src/lib/bike-utils.spec.ts
@@ -0,0 +1,7 @@
+import { bikeUtils } from './bike-utils';
+
+describe('bikeUtils', () => {
+  it('should work', () => {
+    expect(bikeUtils()).toEqual('bike-utils');
+  });
+});
---
+++ libs/bike-utils/src/lib/bike-utils.ts
@@ -0,0 +1,7 @@
+export function bikeUtils(): string {
+  return 'bike-utils';
+}
+
+export function validateBikeSerialNumber(serialNumber: number) {
+  return serialNumber === 123456;
+}
